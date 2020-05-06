c638b0f70206dc0de0dfd657fe9aa9e46be465b2
ajinkyarox <rairikar.ajinkya@gmail.com>
2020-05-06 15:18:25 +0530

    Popup form fields

--- Angular-Node/ecrec/src/app/createaccount/createaccount.component.ts
+++ Angular-Node/ecrec/src/app/createaccount/createaccount.component.ts
@@ -7,10 +7,25 @@ import {MatDialogRef, MAT_DIALOG_DATA, MatDialog} from  '@angular/material/dialo
   styleUrls: ['./createaccount.component.css']
 })
 export class CreateaccountComponent implements OnInit {
-
+username='';
+password='';
+repassword='';
   constructor(private  dialogRef:  MatDialogRef<CreateaccountComponent>, @Inject(MAT_DIALOG_DATA) public  data:  any) { }
 
   ngOnInit(): void {
+  }
+
+  onKeyUsername(event){
+    this.username=event.target.value;
+  }
+  onKeyPassword(event){
+    this.password=event.target.value;
+  }
+  onKeyResPassword(event){
+    this.repassword=event.target.value;
+  }
+  createAccount(){
+    
   }
 closeMe(){
   this.dialogRef.close();
